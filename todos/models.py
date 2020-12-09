f89da6d6bb80b234ed4a5d7a3aa406352175f9c3
study-hary-id <haryansyah.study.id@gmail.com>
2020-12-09 12:32:39 +0700

    Create model for the application Made migrations from the model that has been created

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Todos(models.Model):
+	title = models.TextField(max_length=200)
+	status = models.BooleanField(default=False)
+	
+	
+	def __str__(self):
+		return "%s: %s" % {self.id, self.title}
+
