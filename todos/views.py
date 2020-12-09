2880f9ab614dbc7f4ec1e8b7f1c37fda9a996f43
study-hary-id <haryansyah.study.id@gmail.com>
2020-12-09 13:23:51 +0700

    Added variable to 'active()' and 'completed()' Added logical condition to adjust the border color

--- todos/views.py
+++ todos/views.py
@@ -2,24 +2,38 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Todos
 
+
 def index(request):
 	items = Todos.objects.order_by('-id')
-	return render(request, 'todos/index.html', {'items': items})
+	return render(request, 'todos/index.html', 
+		{'items': items})
+
 
 def active(request):
-	return render(request, 'todos/index.html')
+	items = Todos.objects.filter(status=False) \
+				.order_by('-id')
+	return render(request, 'todos/index.html', 
+		{'items': items})
+
 
 def completed(request):
-	return render(request, 'todos/index.html')
+	items = Todos.objects.filter(status=True) \
+				.order_by('-id')
+	return render(request, 'todos/index.html', 
+		{'items': items})
+
 
 def clear_completed(request):
 	return render(request, 'todos/index.html')
 
+
 def create(request):
 	return render(request, 'todos/index.html')
 
+
 def update(request, id):
 	return render(request, 'todos/index.html')
+
 
 def delete(request, id):
 	return render(request, 'todos/index.html')
