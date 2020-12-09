e29d2c7e18d41b1d9d8d1152a14f08aacc71fa4c
study-hary-id <haryansyah.study.id@gmail.com>
2020-12-09 10:16:04 +0700

    Added some new views to web Set the path in the todos urlpatterns

--- website/todos/urls.py
+++ website/todos/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('active/', views.active, name='active'),
+	path('completed/', views.completed, name='completed'),
+	path('clear_completed/', views.clear_completed, name='clear_completed'),
+	path('create/', views.create, name='create'),
+	path('<int:id>/update/', views.update, name='update'),
+	path('<int:id>/delete/', views.delete, name='delete'),
 ]
--- website/todos/views.py
+++ website/todos/views.py
@@ -4,3 +4,21 @@ from django.http import HttpResponse
 def index(request):
 	return render(request, 'todos/index.html')
 
+def active(request):
+	return render(request, 'todos/index.html')
+
+def completed(request):
+	return render(request, 'todos/index.html')
+
+def clear_completed(request):
+	return render(request, 'todos/index.html')
+
+def create(request):
+	return render(request, 'todos/index.html')
+
+def update(request, id):
+	return render(request, 'todos/index.html')
+
+def delete(request, id):
+	return render(request, 'todos/index.html')
+
